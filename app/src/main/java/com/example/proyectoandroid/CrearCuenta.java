0ec1a8c48e7ebb976c8180dd9f7394fdc51f7639
HP196\CES <informatica@escuelaces.com>
2022-01-17 13:45:05 +0100

    Second commit

--- app/src/main/java/com/example/proyectoandroid/CrearCuenta.java
+++ app/src/main/java/com/example/proyectoandroid/CrearCuenta.java
@@ -2,13 +2,25 @@ package com.example.proyectoandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CrearCuenta extends AppCompatActivity {
 
+    Button crearSesion;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crear_cuenta);
+        crearSesion = findViewById(R.id.Crear);
+        crearSesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(CrearCuenta.this,MainActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/proyectoandroid/MainActivity.java
+++ app/src/main/java/com/example/proyectoandroid/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.proyectoandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -20,7 +21,8 @@ public class MainActivity extends AppCompatActivity {
         Reg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                
+                Intent i = new Intent(MainActivity.this,CrearCuenta.class);
+                startActivity(i);
 
 
             }
