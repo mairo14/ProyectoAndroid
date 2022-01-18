19284f8a7021f8d586efef018f55fb95625deaca
HP196\CES <informatica@escuelaces.com>
2022-01-18 11:41:36 +0100

    alex(18/01/2022)

--- app/src/main/java/com/example/proyectoandroid/Portada.java
+++ app/src/main/java/com/example/proyectoandroid/Portada.java
@@ -1,20 +1,42 @@
 package com.example.proyectoandroid;
 
+import static android.os.SystemClock.sleep;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class Portada extends AppCompatActivity {
-
-    ImageView imageView;
-
+    Handler handler = new Handler();
+    ImageView img;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_portada);
-        imageView= findViewById(R.id.gif);
+        img= findViewById(R.id.gif);
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(Portada.this,MainActivity.class);
+                startActivity(i);
+            }
+        },5000);
+
+
 
 
     }
+
+
+
+
+
+
+
 }
