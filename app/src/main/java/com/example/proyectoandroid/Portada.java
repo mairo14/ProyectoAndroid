5aecae77bfefaedeb8e832ad73d60b38efe92442
HP196\CES <informatica@escuelaces.com>
2022-01-26 11:26:03 +0100

    telefonos comunidad de madrid

--- app/src/main/java/com/example/proyectoandroid/Portada.java
+++ app/src/main/java/com/example/proyectoandroid/Portada.java
@@ -14,11 +14,14 @@ import android.widget.Toast;
 
 public class Portada extends AppCompatActivity {
     Handler handler = new Handler();
-    ImageView img;
+    ImageView img , img2,img3,img4;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_portada);
+        img2 = findViewById(R.id.imageView2);
+        img3 = findViewById(R.id.imageView3);
+        img4 = findViewById(R.id.imageView4);
         img= findViewById(R.id.gif);
         handler.postDelayed(new Runnable() {
             @Override
@@ -28,7 +31,34 @@ public class Portada extends AppCompatActivity {
             }
         },10000);
 
+img2.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+        if (img2.isClickable())
+        {
+            Toast.makeText(Portada.this, "Teléfono : 012", Toast.LENGTH_SHORT).show();
+        }
+    }
+});
+img3.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+        if (img3.isClickable())
+        {
+            Toast.makeText(Portada.this, "Teléfono : 900 102 112", Toast.LENGTH_SHORT).show();
+        }
+    }
+});
 
+img4.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+        if (img4.isClickable())
+        {
+            Toast.makeText(Portada.this, "Teléfono : 112", Toast.LENGTH_SHORT).show();
+        }
+    }
+});
 
 
     }
