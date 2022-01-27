e66183e0a7660bf6dbe47f50cf76b693118c1740
HP196\CES <informatica@escuelaces.com>
2022-01-27 11:07:36 +0100

    commit antes de merge

--- app/src/main/java/com/example/proyectoandroid/MainActivity.java
+++ app/src/main/java/com/example/proyectoandroid/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.proyectoandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
