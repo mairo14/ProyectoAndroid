bf60e88a56259a3b9663a6d3381ad3f31c4e0821
HP196\CES <informatica@escuelaces.com>
2022-01-27 09:42:27 +0100

    iconos reparados

--- app/src/main/java/com/example/proyectoandroid/MainActivity.java
+++ app/src/main/java/com/example/proyectoandroid/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -18,6 +19,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Reg = findViewById(R.id.Registro);
         iniciar = findViewById(R.id.IniciarSesion);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().setNavigationBarColor(getResources().getColor(R.color.black));
+        }
         Reg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
