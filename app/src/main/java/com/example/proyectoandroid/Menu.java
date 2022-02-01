8ff30c499aee14065f12c1448bb23b7fd5b38a26
HP196\CES <informatica@escuelaces.com>
2022-02-01 13:29:20 +0100

    youtube spinner

--- app/src/main/java/com/example/proyectoandroid/Menu.java
+++ app/src/main/java/com/example/proyectoandroid/Menu.java
@@ -3,6 +3,7 @@ package com.example.proyectoandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -15,6 +16,7 @@ import pl.droidsonroids.gif.GifImageView;
 public class Menu extends AppCompatActivity {
   ImageView img1,img2,img3,img4;
   Spinner spinner;
+  private String url;
 
 
     @Override
@@ -24,19 +26,40 @@ public class Menu extends AppCompatActivity {
         setContentView(R.layout.activity_menu);
         img1=findViewById(R.id.metro);
         spinner = findViewById(R.id.spinner);
+        url = "https://www.youtube.com/user/el7desol";
+        Uri uri = Uri.parse(url);
         img1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (img1.isClickable())
                 {
-                    Intent i = new Intent(Menu.this,Metro.class);
-                    startActivity(i);
+                  Intent i = new Intent(Menu.this,Metro.class);
+                   startActivity(i);
                 }
             }
         });
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.opciones, android.R.layout.simple_spinner_item);
         spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id)
+            {
+                String selectedItem = parent.getItemAtPosition(position).toString();
+                if (selectedItem.equals("Youtube"))
+                {
 
+                    Intent i = new Intent(Intent.ACTION_VIEW,uri);
+                    startActivity(i);
+                }
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent)
+            {
+
+            }
+        });
 
 
     }
