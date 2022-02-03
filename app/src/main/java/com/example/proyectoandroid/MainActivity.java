00b477619b76d6bbb1140f551b184ab5cc83ed96
HP196\CES <informatica@escuelaces.com>
2022-02-03 11:42:56 +0100

    CheckBox

--- app/src/main/java/com/example/proyectoandroid/MainActivity.java
+++ app/src/main/java/com/example/proyectoandroid/MainActivity.java
@@ -1,15 +1,26 @@
 package com.example.proyectoandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 
 public class MainActivity extends AppCompatActivity {
 
     Button Reg, iniciar;
+    SharedPreferences preferences;
+    SharedPreferences.Editor editor;
+    String llave = "sesion";
+    CheckBox recordar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +30,9 @@ public class MainActivity extends AppCompatActivity {
         iniciar = findViewById(R.id.IniciarSesion);
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             getWindow().setNavigationBarColor(getResources().getColor(R.color.black));
+        }
+        if (revisarSesion()){
+            startActivity(new Intent(this, RecuperarContrasenia.class));
         }
 
 
@@ -36,6 +50,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent i = new Intent(MainActivity.this,ListadoRes.class);
+                guardarSesion(recordar.isChecked());
                 startActivity(i);
             }
         });
@@ -48,5 +63,21 @@ public class MainActivity extends AppCompatActivity {
         else{
             getDelegate().setLocalNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         }
+    }
+
+    private boolean revisarSesion(){
+        boolean sesion = this.preferences.getBoolean(llave, false);
+        return false;
+    }
+
+    private void guardarSesion(boolean checked){
+        editor.putBoolean(llave,checked);
+        editor.apply();
+    }
+
+    private void inicializarElemento(){
+        preferences = this.getPreferences(Context.MODE_PRIVATE);
+        editor = preferences.edit();
+        recordar = findViewById(R.id.recordarcontraseña);
     }
 }
